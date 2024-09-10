16982cda32d7be1ebcbd38f231e502d6a8659156
TonyTran03 <tony.tran03@hotmail.com>
2024-09-10 15:57:11 -0400

    initial commit

---
+++ electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000' // If in development, load the Next.js dev server
+      : `file://${path.join(__dirname, '../out/index.html')}` // If in production, load the built app
+  );
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
